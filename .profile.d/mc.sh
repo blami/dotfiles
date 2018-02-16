b91b39826cc16252c81bb3af2d0056c7610a3a4e
Ondrej Balaz <blami@blami.net>
2018-02-16 03:45:10 -0500

    Remove Bash related stuff (migrating it to Zsh).

--- .profile.d/mc.sh
+++
@@ -1,7 +0,0 @@
-# ~/.profile.d/mc.sh - Midnight Commander wrapper
-
-[ -n "${BASH_VERSION}${KSH_VERSION}${ZSH_VERSION}" ] || return 0
-
-for D in /usr/lib /usr/libexec ; do
-	[ -r $D/mc/mc-wrapper.sh ] && alias mc=". $D/mc/mc-wrapper.sh"
-done
