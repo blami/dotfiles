7d10669830bae086732d3a091f7734d87fe44763
Ondrej Balaz <blami@blami.net>
2018-04-21 15:06:12 +0100

    Add local profile settings.

---
+++ .profile.d/colorless.sh
@@ -0,0 +1,15 @@
+# ~/.profile.d/colorless.sh - Colorize less output
+
+# Skip configuration for non-interactive shells
+[ -z "$PS1" ] && return
+# Disable colors
+[ ! -z "$NO_COLORLESS" ] && return
+
+# Source less colors
+for colors in "$HOME/.LESS_COLORS.$TERM" \
+	"$HOME/.LESS_COLORS" \
+	"/etc/LESS_COLORS.$TERM" \
+	"/etc/LESS_COLORS"; do
+
+	[ -r "$colors" ] && source $colors && break
+done
