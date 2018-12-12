ca8df60bb9b951753d5ebe96766716c31a804e19
Ondrej Balaz <blami@blami.net>
2018-12-12 20:35:07 +0900

    Use ~/ws/go as GOPATH.

--- .profile.d/wsl.sh
+++ .profile.d/wsl.sh
@@ -3,6 +3,10 @@
 # NOTE: This also expects $HOME to be same as Windows user %HOME%
 
 if [ ! -z "$WSL" ]; then
+
+# Add ~/bin/wsl
+[ -d ~/bin/wsl ] && pathmunge ~/bin/wsl after
+
 # Add Windows utilities (e.g. fsutil, wslconf...) to PATH
 for dir in "/mnt/c/Windows/System32" \
     "/mnt/c/Windows" \
@@ -12,6 +16,15 @@ for dir in "/mnt/c/Windows/System32" \
 
     [ -d $dir ] && pathmunge $dir after
 done
+
+# Add development tools in C:\Devel
+for dir in "/mnt/c/Devel/cmake/bin" \
+    "/mnt/c/Devel/go/bin" \
+    "/mnt/c/Devel/nodejs/bin"; do
+
+    [ -d $dir ] && pathmunge $dir after
+done
+
 builtin unset -v dir
 fi
 
