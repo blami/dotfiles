15021ded89b08b2136e7898d73d39c1eec2f3453
Ondrej Balaz <blami@blami.net>
2019-05-07 12:55:14 +0900

    Add support for Node.js local package installation.

---
+++ .profile.d/node.sh
@@ -0,0 +1,12 @@
+# ~/.profile.d/node.sh - Node.js related environment
+# NOTE: This uses pathmunge
+
+# Add ~/.node/bin to PATH
+[ -d ~/.node/bin ] && pathmunge ~/.node/bin after
+
+# Add ~/.node to NODE_PATH
+NODE_PATH="$HOME/.node/lib/node_modules:$NODE_PATH"
+export NODE_PATH
+
+
+# vim:set ft=zsh:
