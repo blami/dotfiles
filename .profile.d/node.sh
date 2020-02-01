880e81a378a4f07fd4b8d820e3ea7223659e053c
Ondrej Balaz <blami@blami.net>
2020-02-01 19:34:22 +0900

    Make Node.js configs better, remove Neovim PPA.
    Adds Yarn APT source and ~/.yarn/bin to PATH if exists. Removes Neovim
    PPA (as I migrated back to Vim).

--- .profile.d/node.sh
+++ .profile.d/node.sh
@@ -3,6 +3,7 @@
 
 # Add ~/.node/bin to PATH
 [ -d ~/.node/bin ] && pathmunge ~/.node/bin after
+[ -d ~/.yarn/bin ] && pathmunge ~/.yarn/bin after
 
 # Add ~/.node to NODE_PATH
 NODE_PATH="$HOME/.node/lib/node_modules:$NODE_PATH"
