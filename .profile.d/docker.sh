cc0ad3b4c3a673aaa6dcc45e605b9ad4dea4725b
Ondrej Balaz <blami@blami.net>
2021-11-06 13:43:43 +0900

    Set DOCKER_HOST only on WSL1 as WSL2 uses integration.

--- .profile.d/docker.sh
+++ .profile.d/docker.sh
@@ -1,7 +1,7 @@
 # ~/.profile.d/docker.sh - Docker client environment
 
-# In WSL just connect to Windows Docker daemon over TCP
-if [ ! -z "$WSL" ]; then
+# In WSL1 just connect to Windows Docker daemon over TCP
+if [ "$WSL" -eq 1 ]; then
     DOCKER_HOST=127.0.0.1:2375
     #DOCKER_TLS_VERIFY=1
     #DOCKER_CERT_PATH=/mnt/C/ProgramData/docker/certs.d
