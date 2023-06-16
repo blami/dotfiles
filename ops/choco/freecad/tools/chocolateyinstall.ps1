3b00fc0e5367dc8f31f6d4af2bf2933bb96ca7cd
Ondrej Balaz <blami@blami.net>
2023-06-16 22:56:35 +0900

    Add KiCad, FreeCAD and blender

---
+++ ops/choco/freecad/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/FreeCAD/FreeCAD/releases/download/0.20.2/FreeCAD-0.20.2-WIN-x64-installer-3.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'FreeCAD*'
+  silentArgs    = '/S /D=C:\Program Files\FreeCAD'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ ops/choco/kicad/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/KiCad/kicad-source-mirror/releases/download/7.0.5/kicad-7.0.5-x86_64.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'KiCad*'
+  silentArgs    = '/S /D=C:\Program Files\KiCad'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
