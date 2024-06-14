8f9e1c82385ea9ec1519d1e5f7ff7dd570e8ac59
Ondrej Balaz <blami@blami.net>
2024-06-14 22:53:52 +0900

    feat: Add Choco packages
    * Autohotkey
    * ImgBurn
    * Irfanview + plugins
    * OBS Studio
    * Total Commander
    * VLC

---
+++ ops/choco/vlc/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://get.videolan.org/vlc/3.0.18/win64/vlc-3.0.18-win64.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'VLC media player*'
+
+  silentArgs    = '/S'
+
+  validExitCodes= @(0, 1223)
+}
+
+# Install to C:\Program Files\VLC instead of VideoLAN\VLC
+mkdir -force HKLM:\Software\VideoLAN\VLC
+Set-ItemProperty HKLM:\Software\VideoLAN\VLC InstallDir "C:\Program Files\VLC"
+
+Install-ChocolateyPackage @packageArgs
