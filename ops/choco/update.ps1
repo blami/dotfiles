93d11a09c00857b29f44d7fd343335f2def35737
Ondrej Balaz <blami@blami.net>
2024-07-09 01:28:09 +0900

    feat: Add bulk updater for Chocolatey-AU

---
+++ ops/choco/update.ps1
@@ -0,0 +1,16 @@
+﻿<#
+.SYNOPSIS
+  Update all packages to the latest version using Chocolatey-AU
+#>
+
+$ErrorActionPreference = 'Stop' # stop on all errors
+
+Get-ChildItem -Path $PSScriptRoot -Directory | ForEach-Object {
+    $updatePs1 = (Join-Path -Path $_.FullName -ChildPath update.ps1)
+    if (Test-Path -Path $updatePs1 -Type Leaf) {
+        # Run update.ps1
+        Push-Location $_.FullName
+        & $updatePs1
+        Pop-Location
+    }
+}
