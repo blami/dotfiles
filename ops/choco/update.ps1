5590ec770dafa40885c8878d86d6042c8cb2fbf9
Ondrej Balaz <blami@blami.net>
2024-07-22 23:30:28 +0900

    feat: Use alias from chocolatey-AU

--- ops/choco/update.ps1
+++ ops/choco/update.ps1
@@ -2,15 +2,7 @@
 .SYNOPSIS
   Update all packages to the latest version using Chocolatey-AU
 #>
+Import-Module Chocolatey-AU
 
 $ErrorActionPreference = 'Stop' # stop on all errors
-
-Get-ChildItem -Path $PSScriptRoot -Directory | ForEach-Object {
-    $updatePs1 = (Join-Path -Path $_.FullName -ChildPath update.ps1)
-    if (Test-Path -Path $updatePs1 -Type Leaf) {
-        # Run update.ps1
-        Push-Location $_.FullName
-        & $updatePs1
-        Pop-Location
-    }
-}
+$au_root = "\\wsl.localhost\debian\home\blami\ops\choco" ; Update-AUPackages
