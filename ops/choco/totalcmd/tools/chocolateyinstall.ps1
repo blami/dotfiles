a8e8c0e8507a0c13b73a386085d295e71d058290
Ondrej Balaz <blami@blami.net>
2024-07-22 23:31:07 +0900

    fix: Upgrade Total Commander

--- ops/choco/totalcmd/tools/chocolateyinstall.ps1
+++ ops/choco/totalcmd/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop' # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://totalcommander.ch/1052/tcmd1052x64.exe'
+$url        = 'https://totalcommander.ch/1052/tcmd1103x64.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
