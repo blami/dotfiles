c860f0503dc310e969f5a9a470492cc95f8da03e
Ondrej Balaz <blami@blami.net>
2024-07-22 23:31:38 +0900

    fix: Upgrade VLC

--- ops/choco/vlc/tools/chocolateyinstall.ps1
+++ ops/choco/vlc/tools/chocolateyinstall.ps1
@@ -1,8 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop' # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-(Invoke-WebRequest -URI "https://get.videolan.org/vlc/last/win64/").Content -match 'vlc-([0-9]+\.[0-9]+\.[0-9]+)-win64.exe'
-$url        = "https://get.videolan.org/vlc/last/win64/vlc-$($matches[1])-win64.exe"
+$url        = "https://get.videolan.org/vlc/last/win64/vlc-3.0.21-win64.exe"
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -19,5 +18,7 @@ $packageArgs = @{
 # Install to C:\Program Files\VLC instead of VideoLAN\VLC
 mkdir -force HKLM:\Software\VideoLAN\VLC
 Set-ItemProperty HKLM:\Software\VideoLAN\VLC InstallDir "C:\Program Files\VLC"
+
+
 
 Install-ChocolateyPackage @packageArgs
