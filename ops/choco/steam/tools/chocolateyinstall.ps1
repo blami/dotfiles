7054ad0e7513edd915a854a4524251df6adc3ab1
Ondrej Balaz <blami@blami.net>
2024-07-25 02:52:50 +0900

    feat: Add Steam client

---
+++ ops/choco/steam/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'Steam*'
+
+  silentArgs    = '/S /D=C:\Games\Steam'
+
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
