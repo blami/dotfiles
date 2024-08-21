79573da0d4ce9cf9977b59fd9d8ef9600b33108a
Ondrej Balaz <blami@blami.net>
2024-08-21 21:52:10 +0900

    feat: Add Axialis IconWorkshop

---
+++ ops/choco/iconworkshop/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64      = "https://www.axialis.com/downloads/IconWorkshop-Pro.exe"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url64         = $url64
+  softwareName  = 'Axialis IconWorkshop*'
+
+  silentArgs    = '/q /n /f "C:\Program Files\IconWorkshop"'
+
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
