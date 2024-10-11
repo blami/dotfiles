57d9a4d432ebca92623a47022792d53938f11126
Ondrej Balaz <blami@blami.net>
2024-10-11 13:08:48 -0700

    feat: Add Google Drive and Foxit PDF Editor

---
+++ ops/choco/foxitpdfeditor/tools/chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url32      = 'https://cdn01.foxitsoftware.com/product/phantomPDF/desktop/win/11.2.10/FoxitPDFEditor11210_enu_Setup_Website.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url32
+  softwareName  = 'Foxit PDF Editor*'
+
+  silentArgs    = '/install /quiet /norestart /noshortcut /DisableInternet /DIR "C:\Program Files (x86)\Foxit PDF Editor"'
+
+  validExitCodes = @(0)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ ops/choco/googledrive/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://dl.google.com/drive-file-stream/GoogleDriveSetup.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'Google Drive*'
+
+  silentArgs    = '--silent --gsuite_shortcuts=false --skip_launch_new'
+
+  validExitCodes= @(0)
+}
+
+# Advanced configuration
+mkdir -force HKCU:\Software\Google\DriveFS > $null
+Set-ItemProperty HKCU:\Software\Google\DriveFS DefaultMountPoint "%USERPROFILE%\AppData\GoogleDrive" -Type ExpandString
+Set-ItemProperty HKCU:\Software\Google\DriveFS AutomaticErrorReporting 0
+Set-ItemProperty HKCU:\Software\Google\DriveFS PromptToBackupDevices 0
+Set-ItemProperty HKCU:\Software\Google\DriveFS SearchHotKey 0
+
+Install-ChocolateyPackage @packageArgs
